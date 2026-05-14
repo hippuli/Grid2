0d1b8267dca61fc807ea387974b789dc4fbf5a5d
Konsta Niinimaki <konsta.niinimaki@servur.org>
2026-05-14 19:10:35 +0300

    first commit

---
+++ Grid2/locales/koKR.lua
@@ -0,0 +1,126 @@
+local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2", "koKR")
+if not L then return end
+
+--{{{ Actually used
+L["Border"] = "테두리"
+L["Charmed"] = "현혹"
+L["Default"] = "기본값"
+L["Grid2"] = "Grid2"
+L["Beast"] = "야수"
+L["Demon"] = "악마"
+L["Humanoid"] = "인간형"
+L["Elemental"] = "정령"
+--}}}
+
+--{{{ Health
+L["DEAD"] = "죽음"
+L["GHOST"] = "유령"
+L["FD"] = "죽은척"
+L["Offline"] = "오프라인"
+--}}}
+
+--{{{ Pvp
+L["PvP"] = "PvP"
+L["FFA"] = "FFA"
+--}}}
+
+--{{{ Range
+L["OOR"] = "범위 밖"
+--}}}
+
+--{{{ ReadyCheck
+L["?"] = "?"
+L["R"] = "O"
+L["X"] = "X"
+L["AFK"] = "자리비움"
+--}}}
+
+--{{{ Target
+L["target"] = "대상"
+--}}}
+
+--{{{ Vehicle
+L["vehicle"] = "탈것"
+--}}}
+
+--{{{ VoiceComm
+L["talking"] = "대화 중"
+--}}}
+
+--{{{ DungeonRole
+L["TANK"] = "방어 전담"
+L["HEALER"] = "치유 전담"
+L["DAMAGER"] = "공격 전담"
+--}}}
+
+--{{{ SpecRole
+L["Tank"] = "탱커"
+L["Healer"] = "힐러"
+L["Melee"] = "근접"
+L["Ranged"] = "원거리"
+--}}}
+
+--{{{ Other roles
+L["RL"] = "공격대장"
+L["RA"] = "부공격대장"
+L["ML"] = "전리품 담당"
+--}}}
+
+--{{{ Resurrection
+L["Reviving"] = "부활 중"
+L["Revived"] = "부활 완료"
+---}}}
+
+--{{{ Self
+L["Me"] = "나"
+---}}}
+
+--{{{ Combat
+L["combat"] = "전투"
+---}}}
+
+--{{{ Summon
+L["Pending"] = "대기 중"
+L["Accepted"] = "수락함"
+L["Declined"] = "거절됨"
+---}}}
+
+--Layouts
+L["None"] = "없음"
+L["Solo"] = "솔로잉"
+L["Solo w/Pet"] = "솔로잉 (+소환수)"
+L["Party"] = "파티"
+L["Party w/Pets"] = "파티 (+소환수)"
+L["By Role"] = "역할별"
+L["By Role w/Pets"] = "역할별 (+소환수)"
+L["By Group"] = "그룹별"
+L["By Group w/Pets"] = "그룹별 (+소환수)"
+L["By Group w/Tanks"] = "그룹별 (방어 전담 포함)"
+L["By Group & Role"] = "그룹 & 역할별"
+L["By Class"] = "직업별"
+L["By Class w/Pets"] = "직업별 (+소환수)"
+L["By Class | 1x25"] = "직업별 | 1x25"
+L["By Class | 2x15"] = "직업별 | 2x15"
+L["By Group | 40"] = "그룹별 | 40"
+L["By Group | 40 w/Pets"] = "그룹별 | 40 (+소환수)"
+L["By Group | 4x10"] = "그룹별 | 4x10"
+L["By Group | 4x10 w/Pets"] = "그룹별 | 4x10 (+소환수)"
+L["By Role | 10x4"] = "역할별 | 10x4"
+L["Select Layout"] = "배치 선택"
+L["By Group & Class"] = "그룹 & 직업별"
+L["By Group & Class w/Pets"] = "그룹 & 직업별 (+소환수)"
+L["By Group & Role w/Pets"] = "그룹 & 역할별 (+소환수)"
+
+--LDB
+L["Profile"] = "프로필"
+L["Theme"] = "테마"
+L["Layout"] = "배치"
+L["Select Layout"] = "배치 선택"
+L["Never"] = "안 함"
+L["Raid"] = "레이드"
+L["Grouped"] = "그룹화"
+L["Always"] = "항상"
+L["Grid2 Visibility"] = "Grid2 표시"
+L["Grid2 Movement"] = "Grid2 이동"
+L["Locked"] = "잠금"
+L["|cFFff4040Left Click|r to open configuration\n|cFFff4040Right Click|r to open layouts menu"] = "|cFFff4040왼쪽 클릭|r으로 설정 열기\n|cFFff4040오른쪽 클릭|r으로 배치 메뉴 열기"
